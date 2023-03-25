4640cf4d57a990991d1a855f93b9ccf2b0361820
Airton <airton.vi.fe@hotmail.com>
2023-03-24 22:56:14 -0300

    Clear branch

--- app/src/main/java/com/example/anamnesedrapp/usuario/service/dto/UsuarioDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.example.anamnesedrapp.usuario.service.dto
-
-data class UsuarioDTO (
-    val id: String,
-    val nomeUsuario: String,
-    val criadoPor: String
-)
--- app/src/main/java/com/example/anamnesedrapp/usuario/ui/vp/RegistraVP.kt
+++
@@ -1,9 +0,0 @@
-package com.example.anamnesedrapp.usuario.ui.vp
-
-import com.example.anamnesedrapp.usuario.service.dto.UsuarioDTO
-
-data class RegistraVP(
-    val nomeUsuario: String = "",
-    val senha: String = "",
-    val criadoPor: String
-)
